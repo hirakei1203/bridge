c7c2ce7f305bd85199f073b7d2fe471e6268935e
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-03-08 14:24:10 +0900

    select box

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,25 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Dentist / Patient') }}</label>
+
+                            <div class="col-md-6">
+                                <!-- <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="role" value="{{ old('role') }}" required autocomplete="name" autofocus> -->
+                                <!-- <label for="exampleFormControlSelect1">Example select</label> -->
+                                <select id="name" type="text" class="form-control @error('name') is-invalid @enderror" id="exampleFormControlSelect1" name="role" value="{{ old('role') }}">
+                                    <option>Dentist</option>
+                                    <option>Patinet</option>
+                                </select>
+
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
