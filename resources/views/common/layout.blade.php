f5ab66e50d7c113de7cbbbfa51c1844384e22120
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-03-11 23:25:18 +0900

    scss

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -3,13 +3,14 @@
   <head>
   <meta charset="utf-8">
   <title>@yield('title')</title>
-  <link href="css/style.css" rel="stylesheet" type="text/css">
+
+  <!-- css,scssの呼び出し -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
   <!-- <bootstrapの呼び出し> -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <link href="/css/sticky-footer.css" rel="stylesheet" media="screen">
 
   <!-- jsファイルの呼び出し -->
-  <script src="js/common.js" type="text/javascript"></script>
+  <!-- <script src="js/common.js" type="text/javascript"></script> -->
   </head>
 
     <body>
@@ -18,7 +19,7 @@
      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
     </nav>
 
-    <div class="container-fluid">
+    <div class="container-fluid side-bar">
       <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
           <div class="sidebar-sticky">
@@ -32,31 +33,25 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="file"></span>
-                  Orders
+                  My patients
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="shopping-cart"></span>
-                  Products
+                  My dentists
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="users"></span>
-                  Customers
+                  Dentists Ranking
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
+                  Contact to support
                 </a>
               </li>
             </ul>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-
+<div class="greeen">all green</div>
 
 
 
