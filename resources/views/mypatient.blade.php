08b0a7cfc70364ac8edbc7a873ad8b57e65fd417
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-03-31 12:53:09 +0900

    mypatien image

--- resources/views/mypatient.blade.php
+++ resources/views/mypatient.blade.php
@@ -11,18 +11,21 @@
       <a class="mypatients-container__link-box__link" href="{{ url('mypatient') }}">Registaration of new patient</a>
     </div> -->
 
+  @foreach($patients_in_charge as $patient)
+
     <div class="mypatient-wrapper">
       <div class="mypatient-wrapper__image-box">
         <div class="mypatient-wrapper__image-box__circle">
+          <img class="patient_image" src="{{ asset('storage/images/'.$patient->image) }}">
         </div>
       </div>
       <div class="mypatient-wrapper__name-box">
-        <h4 class="mypatient-wrapper__name-box__name">Patient's name:<br>Mickeal Jackson</h4>
-        <h5 class="mypatient-wrapper__name-box__age">Age: 29</h5>
+        <h4 class="mypatient-wrapper__name-box__name">Patient's name:<br>{{ $patient->name }}</h4>
+        <h5 class="mypatient-wrapper__name-box__age">Age: {{ $patient->age }}</h5>
       </div>
 
       <div class="mypatient-wrapper__info-box">
-        <h5 class="mypatient-wrapper__info-box__issue-frame">Current issue:<br></h5>
+        <h5 class="mypatient-wrapper__info-box__issue-frame">Current issue: {{ $patient->sympton }}<br></h5>
       </div>
       
       <div class="mypatient-wrapper__date-box">
@@ -37,32 +40,7 @@
       </div>
     </div>
 
-<!-- mypatient二回目box -->
-    <div class="mypatient-wrapper">
-      <div class="mypatient-wrapper__image-box">
-        <div class="mypatient-wrapper__image-box__circle">
-        </div>
-      </div>
-      <div class="mypatient-wrapper__name-box">
-        <h4 class="mypatient-wrapper__name-box__name">Patient's name:<br>Mickeal Jackson</h4>
-        <h5 class="mypatient-wrapper__name-box__age">Age: 29</h5>
-      </div>
-
-      <div class="mypatient-wrapper__info-box">
-        <h5 class="mypatient-wrapper__info-box__issue-frame">Current issue:<br></h5>
-      </div>
-      
-      <div class="mypatient-wrapper__date-box">
-        <h5 class="mypatient-wrapper__date-box__last-date">Last treatment date:</h5>
-        <h5 class="mypatient-wrapper__date-box__last-content">Last treatment:</h5>
-        <h5 class="mypatient-wrapper__date-box__next-date">Next date: </h5>
-      </div>
-
-      <div class="mypatient-wrapper__message-box">
-        <h5 class="mypatient-wrapper__message-box__notification">！</h5>
-        <h5 class="mypatient-wrapper__message-box__message-link">メッセージを見る</h5>
-      </div>
-    </div>
+    @endforeach
 
 </div>
 <!-- endforeach -->
